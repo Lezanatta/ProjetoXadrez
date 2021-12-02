2e5e165948e825dd090f5dc7c07025b17f6ebb12
Leandro Zanatta <Leandrozanatta7@gmail.com>
2021-12-02 20:12:14 -0300

    Atualiza a promocao da peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,8 +41,12 @@ public class Program {
 				}
 				
 				if(chessMatch.getPromoted() != null) {
-					System.out.println("Entre com a peca a ser promovida: (B/N/R/Q)");
-					String type = sc.nextLine();
+					System.out.print("Entre com a peca a ser promovida (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R")&& !type.equals("Q")) {
+						System.out.print("Valor invalido! Entre com a peca a ser promovida (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
