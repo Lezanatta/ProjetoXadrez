39991866f0e9482dfe2295c019bf970ff8e37976
Leandro Zanatta <Leandrozanatta7@gmail.com>
2021-12-02 10:01:32 -0300

    Implementa movimentos possiveis

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -21,6 +21,7 @@ public class Rook extends ChessPiece{
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0,0);
+		
 		//PARA CIMA
 		p.setValues(position.getRow() -1 , position.getColumn());
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPice(p)){
@@ -47,7 +48,7 @@ public class Rook extends ChessPiece{
 		p.setValues(position.getRow() , position.getColumn() + 1);
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPice(p)){
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() +1 );
+			p.setColumn(p.getColumn() + 1);
 		}
 		
 		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
